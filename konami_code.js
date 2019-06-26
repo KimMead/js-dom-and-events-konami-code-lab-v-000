bfad1e93b1f1bbc79c81219c407645582ee309e1
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-06-26 04:21:18 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -5,16 +5,16 @@ function init() {
   let index = 0;
 
   document.body.addEventListener('keydown', function(e)) {
-    const key = parseInt(e.detail || e.which); 
+    const key = parseInt(e.detail || e.which);
 
     if (key === code[index]){
-      index++; 
+      index++;
       if (index == code.length){
-        alert("Hurray!"); 
-        index = 0; 
+        alert("Hurray!");
+        index = 0;
       }
     } else {
-      index = 0; 
+      index = 0;
     }
   });
 }
